fb7774bbefed6043d464cc5b9e4c7b83c336df2e
Michael Wittig <post@michaelwittig.info>
2015-05-11 19:29:53 +0200

    NFS install script

---
+++ chapter8/nfs-server-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+yum -y install nfs-utils nfs-utils-lib
+service rpcbind start
+service nfs start
+
+echo "/media/ephemeral0 *(rw,async)" >> /etc/exports
+exportfs -a
+
+# mount EBS backup volume
+mkdir /mnt/backup
+echo "/dev/xvdf /mnt/backup ext4 defaults,nofail 0 2" >> /etc/fstab
+mount -a
+
+# backup cron
+cat > /etc/cron.d/backup << EOF
+SHELL=/bin/bash
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/opt/aws/bin
+MAILTO=root
+HOME=/
+0,15,30,45 * * * * rsync -av --delete --exclude /media/ephemeral0/ /mnt/backup/ ; fsfreeze -f /mnt/backup/ ; aws --region $REGION ec2 create-snapshot --volume-id $VOLUMEID --description "NFS backup"; fsfreeze -u /mnt/backup/
+EOF
