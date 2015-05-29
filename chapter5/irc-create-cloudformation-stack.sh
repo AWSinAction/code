820c2ff8661b0239107c591611026914d3b4803a
Michael Wittig <post@michaelwittig.info>
2015-05-29 20:05:24 +0200

    wait until complete

--- chapter5/irc-create-cloudformation-stack.sh
+++ chapter5/irc-create-cloudformation-stack.sh
@@ -2,3 +2,8 @@
 
 vpc=$(aws ec2 describe-vpcs --query Vpcs[0].VpcId --output text)
 aws cloudformation create-stack --stack-name irc --template-url https://s3.amazonaws.com/awsinaction/chapter5/irc-cloudformation.json --parameters ParameterKey=VPC,ParameterValue=$vpc
+
+while [[ `aws cloudformation describe-stacks --stack-name irc --query Stacks[0].StackStatus` != *"COMPLETE"* ]]
+do
+	sleep 10
+done
