0e97c08e778e88a836e3bb0ac51b8168c926c792
Michael Wittig <post@michaelwittig.info>
2015-07-12 10:59:59 +0200

    call the state handler method

--- chapter13/worker.js
+++ chapter13/worker.js
@@ -50,12 +50,16 @@ app.get('/', function(request, response) {
 app.post('/sqs', function(request, response) {
   assert.string(request.body.imageId, "imageId");
   assert.string(request.body.desiredState, "desiredState");
+  console.log("request.body", request.body);
   getImage(request.body.imageId, function(err, image) {
     if (err) {
       throw err;
     } else {
-      // TODO
-      response.json(request.body);
+      if (global[request.body.desiredState] === 'function') {
+        global[request.body.desiredState](image, request, response);
+      } else {
+        throw new Error("unsupported desiredState");
+      }
     }
   });
 });
